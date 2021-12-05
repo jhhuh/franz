6329eba12700afc828f7d380aeb911c5e6d3cdc1
HUH, Ji-Haeng <jhhuh.korea@gmail.com>
2021-12-05 11:21:40 +0900

    use packageSourceOverride facility in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,35 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
+with pkgs;
 let
-  myHask = pkgs.ghc.withHoogle (hp: with hp; [
-    haskell-language-server
-    ghcid
-    #
-    bytestring
-    cereal
-    containers
-    concurrent-resource-map
-    cpu
-    deepseq
-    directory
-    exceptions
-    fast-builder
-    filepath
-    fsnotify
-    hashable
-    mtl
-    network
-    optparse-applicative
-    process
-    retry
-    sendfile
-    stm
-    stm-delay
-    text
-    transformers
-    unboxed-ref
-    vector
-    unordered-containers
-  ]);
-in pkgs.mkShell {
-  buildInputs = [ myHask ] ++ (with pkgs; [cabal-install]);
+    myHask = haskellPackages.extend (
+        haskell.lib.packageSourceOverrides {
+            franz = ./.;
+        });
+in myHask.shellFor {
+    packages = (hp: with hp; [franz]);
+    withHoogle = true;
+    buildInputs = (with pkgs; [
+        haskell-language-server
+        ghcid
+    ]) ++ (with myHask; [
+        implicit-hie
+    ]);
 }
